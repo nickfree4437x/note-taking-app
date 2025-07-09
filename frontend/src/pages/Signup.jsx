099c135a297f5ef9a0e248c6c5066b1944db486f
nickfree4437x <146987759+nickfree4437x@users.noreply.github.com>
2025-07-09 12:10:55 +0530

    Update Signup.jsx

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -166,7 +166,7 @@ const Signup = () => {
               <input
                 type="text"
                 name="name"
-                className="w-full border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-1 focus:ring-blue-400 pt-3"
+                className="w-full border border-gray-500 rounded-lg px-3 py-2 focus:outline-none focus:ring-1 focus:ring-blue-400 pt-3"
                 value={formData.name}
                 onChange={handleChange}
                 onFocus={() => handleFocus("name")}
